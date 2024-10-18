b31805823622fa02c34c3d59d684c5582080c4d0
Pedro souza <121955360+devpedroafonso@users.noreply.github.com>
2024-10-18 15:31:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const name = document.getElementById("name");
+const startingCapital = document.getElementById("startingCapital");
+const interestRate = document.getElementById("interestRate");
+const numberMonths = document.getElementById("numberMonths");
+
+function calculateAmount(){
+    let capital = parseFloat(startingCapital.value);
+    let interest = parseFloat(interestRate.value)/100;
+    let months = parseInt(numberMonths.value());
+    let calculation = capital*(1+interest)**months;
+    let message = name.value+"no final de "+months+(" meses você tera um capital de "+calculation.toFixed(2));
+    alert(message);
+}
