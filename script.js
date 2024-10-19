d20a7e908095444c94a9903dc5a34000feb98326
Pedro souza <pedroasouza.contato@gmail.com>
2024-10-19 16:11:00 -0300

    atualization

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const numberMonths = document.getElementById("numberMonths");
 function calculateAmount(){
     let capital = parseFloat(startingCapital.value);
     let interest = parseFloat(interestRate.value)/100;
-    let months = parseInt(numberMonths.value());
-    let calculation = capital*(1+interest)**months;
-    let message = name.value+"no final de "+months+(" meses você tera um capital de "+calculation.toFixed(2));
+    let months = parseInt(numberMonths.value);
+    let calculation = capital * Math.pow(1 + interest, months);
+    let message = name.value+"no final de "+months+" meses você tera um capital de R$"+calculation.toFixed(2);
     alert(message);
 }
