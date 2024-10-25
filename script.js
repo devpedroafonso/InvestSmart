3eae80041895e597bf12cac5e80df0234ecc5224
Pedro souza <pedroasouza.contato@gmail.com>
2024-10-24 21:29:38 -0300

    update
    codigo comentado

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const name = document.getElementById("name");
+/*Const é usado para nomear uma constant */
+const nameUser = document.getElementById("name").value;
+/*"document.getElementById() é usado para puxar a informação que foi utilizada no site usando o id"*/
 const startingCapital = document.getElementById("startingCapital");
 const interestRate = document.getElementById("interestRate");
 const numberMonths = document.getElementById("numberMonths");
 
-function calculateAmount(){
+function calculateAmount()/*gera a função que sera puxada para o html*/ {
     let capital = parseFloat(startingCapital.value);
-    let interest = parseFloat(interestRate.value)/100;
+    /*let é uasdo para criar uma variavel que sera usada momentaniamente dentro da função */
+    let interest = parseFloat(interestRate.value) / 100;
     let months = parseInt(numberMonths.value);
-    let calculation = capital * Math.pow(1 + interest, months);
-    
-    
 
-        let message = name.value+" no final de "+months+" meses você tera um capital de R$"+calculation.toFixed(2);
-        message = message.replace(".",",");
-        alert(message);
+    let calculation = capital * Math.pow(1 + interest, months);
+
+    let message = nameUser + " No final de " + months + " meses você tera um capital de " + new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(calculation.toFixed(2)); /*"toFixed()"" é usado para definir quantos numeros aparecerão depois da ,*/
+     /*Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }) foi usado para converter o resultado da variavel para a moeda Brasileira*/
+    alert(message);
 }   
